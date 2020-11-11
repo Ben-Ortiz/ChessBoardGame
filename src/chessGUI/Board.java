9295e022941fda8a689496e220e7274adb6fa3f4
Ben Ortiz <benortiz1391@gmail.com>
2020-11-11 01:38:10 -0500

    starting to make baord and pieces

--- src/chessGUI/Board.java
+++ src/chessGUI/Board.java
@@ -1,13 +1,33 @@
 package chessGUI;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-
+import java.util.ArrayList;
 import javax.swing.JComponent;
+import chessPieces.Piece;
 
+@SuppressWarnings("serial")
 public class Board extends JComponent {
 	
 	public int turnCounter = 0;
 	private static Image NULL_IMAGE = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+	
+	private final int Square_Width = 65;
+	public ArrayList<Piece> White_Pieces;
+	public ArrayList<Piece> Black_Pieces;
+	
+	public ArrayList<DrawingShape> Static_Shapes;
+	public ArrayList<DrawingShape> Piece_Graphics;
+	
+	public Piece Active_Piece;
+	
+	private final int rows = 8;
+	
+	interface DrawingShape {
+	    boolean contains(Graphics2D g2, double x, double y);
+	    void adjustPosition(double dx, double dy);
+	    void draw(Graphics2D g2);
+	}
 
 }
