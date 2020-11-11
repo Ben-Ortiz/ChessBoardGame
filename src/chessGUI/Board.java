33ed7d7249d1175c934314138bff0cef201d52f3
Ben Ortiz <benortiz1391@gmail.com>
2020-11-11 01:28:58 -0500

    working on chessGUI

---
+++ src/chessGUI/Board.java
@@ -0,0 +1,13 @@
+package chessGUI;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JComponent;
+
+public class Board extends JComponent {
+	
+	public int turnCounter = 0;
+	private static Image NULL_IMAGE = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+
+}
